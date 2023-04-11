b843eb789f9ae991003b21faf45eead6c5ec68bc
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-04-11 22:31:03 +0200

    feat: rename configs for v0.12

--- micado/cli.py
+++ micado/cli.py
@@ -15,8 +15,8 @@ DEFAULT_VERS = "v0.11.2"
 CONFIGS: dict = {
     "hosts": ("playbook/inventory", "hosts.yml"),
     "cloud": ("playbook/project/credentials", "credentials-cloud-api.yml"),
-    "registry": ("playbook/project/credentials", "credentials-docker-registry.yml"),
-    "micado": ("playbook/project/credentials", "credentials-micado.yml"),
+    "registry": ("playbook/project/credentials", "credentials-registries.yml"),
+    "web": ("playbook/project/credentials", "credentials-micado.yml"),
     "settings": ("playbook/project/host_vars", "micado.yml"),
 }
 
