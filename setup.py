133f463b18bb5decd1729d742236d3c19043b62d
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2023-10-24 21:34:23 +0100

    chore: bump for release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIREMENTS = [
 
 setup(
     name="micado-client",
-    version="0.12.6",
+    version="0.12.7",
     description="A Python Client Library for MiCADO",
     long_description=long_description,
     long_description_content_type="text/markdown",
