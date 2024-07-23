a8f294cecf354f4ca683464afb85c06edc99a767
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2024-07-23 09:59:19 +0000

    chore: bump version

--- micado/cli.py
+++ micado/cli.py
@@ -15,7 +15,7 @@ from micado import MicadoClient, exceptions
 
 yaml = YAML()
 
-DEFAULT_VERS = "v0.12.3"
+DEFAULT_VERS = "v0.12.5"
 
 class OrderedGroup(click.Group):
     def list_commands(self, ctx) -> list[str]:
