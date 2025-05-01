fa3af86e45df3b6adbf4b6d8c23a2cd1526a02e2
Jay DesLauriers <35102795+jaydesl@users.noreply.github.com>
2025-05-01 16:22:55 +0100

    feat: bump micado vers

--- micado/cli.py
+++ micado/cli.py
@@ -15,7 +15,7 @@ from micado import MicadoClient, exceptions
 
 yaml = YAML()
 
-DEFAULT_VERS = "v0.12.6"
+DEFAULT_VERS = "v0.12.7"
 
 class OrderedGroup(click.Group):
     def list_commands(self, ctx) -> list[str]:
